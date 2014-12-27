813c6ea7f852e4fa8211ce8ee7f3a15ee69aa8ef
Stephanie Barreyro <stephaniebarreyro@gmail.com>
2014-12-27 15:41:16 -0500

    Add Friendship migration.

--- db/migrate/20141218230414_create_friendships.rb
+++ db/migrate/20141218230414_create_friendships.rb
@@ -1,4 +1,9 @@
 class CreateFriendships < ActiveRecord::Migration
   def change
+    create_table :friendships do |t|
+      t.references  :user
+      t.integer     :user2_id
+      t.string      :status
+    end
   end
 end
