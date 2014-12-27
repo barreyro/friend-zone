5c730b7de9c76864eb8d3220bc1f1801de271081
Stephanie Barreyro <stephaniebarreyro@gmail.com>
2014-12-27 15:44:24 -0500

    Add Users table.

--- db/migrate/20141218230359_create_users.rb
+++ db/migrate/20141218230359_create_users.rb
@@ -1,4 +1,12 @@
 class CreateUsers < ActiveRecord::Migration
   def change
+    create-table :users do |t|
+      t.string  :username
+      t.string  :password_digest
+      t.integer :age
+      t.string  :email
+      t.string  :interest
+      t.string  :gender
+    end
   end
 end
