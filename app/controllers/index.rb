06c373612dc8e3960ec3cae27018e8f43113e350
Stephanie Barreyro <stephaniebarreyro@gmail.com>
2014-12-28 16:54:00 -0500

    Implement Sign Up route.

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -3,6 +3,18 @@ get '/' do
   erb :index
 end
 
-get 'signup' do
+get '/signup' do
   erb :sign_up
+end
+
+post '/signup' do
+  puts params[:user]
+  user = User.new(params[:user])
+
+  if user.save
+    session[:user_id] = user.id
+    redirect '/'
+  else
+    redirect '/sign_up'
+  end
 end
