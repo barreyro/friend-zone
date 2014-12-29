b51291ba5c11e49386064035d05ba05a0784cdff
Stephanie Barreyro <stephaniebarreyro@gmail.com>
2014-12-29 00:32:30 -0500

    Login and logout works.

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -9,12 +9,31 @@ end
 
 post '/signup' do
   puts params[:user]
-  user = User.new(params[:user])
+  @user = User.new(params[:user])
 
-  if user.save
-    session[:user_id] = user.id
+  if @user.save
+    session[:user_id] = @user.id
     redirect '/'
   else
-    redirect '/sign_up'
+    redirect '/signup'
   end
+end
+
+get '/login' do
+  erb :login
+end
+
+post '/login' do
+  @user = User.find_by(email: params[:user][:email])
+  if @user && @user.authenticate(params[:user][:password])
+    session[:user_id] = @user.id
+    redirect '/'
+  else
+    redirect '/login'
+  end
+end
+
+get '/logout' do
+  session[:user_id] = nil
+  redirect '/'
 end
